06a5bf0bbcd4a75c2cb5c4c4614440c95dbe941d
Luc Thoi Sang <devbrownbear@gmail.com>
2019-03-29 09:06:23 +0700

    Lesson 4.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Luc Thoi Sang on 3/29/19.
+//  Copyright © 2019 Luc Thoi Sang. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Luc Thoi Sang on 3/29/19.
+//  Copyright © 2019 Luc Thoi Sang. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
