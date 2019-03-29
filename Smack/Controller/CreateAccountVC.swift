c3c228dc5daf24e60e8502c3eb9b16c68d03d771
Luc Thoi Sang <devbrownbear@gmail.com>
2019-03-29 09:45:10 +0700

    Lesson 5

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,22 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Luc Thoi Sang on 3/29/19.
+//  Copyright © 2019 Luc Thoi Sang. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+}
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -19,4 +19,8 @@ class LoginVC: UIViewController {
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func createAccntBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
 }
