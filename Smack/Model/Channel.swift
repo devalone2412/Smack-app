f80194d0f48c926af4f4df521c3a8ba73fb58d4f
Luc Thoi Sang <devbrownbear@gmail.com>
2019-04-01 17:35:01 +0700

    Lesson 19

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Luc Thoi Sang on 4/1/19.
+//  Copyright © 2019 Luc Thoi Sang. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
